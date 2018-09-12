8700f6fa6e660b964f5feb3e7a6b7591d4d60ab4
jjnotjayjay <jj@alumni.usc.edu>
2018-09-12 13:19:39 -0700

    Create a new Car object, passing in the created img. #1

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ class Car {
   }
 }
 
-var $img = document.createElement('img')
-$img.setAttribute('src', 'blue-car.png')
-$img.setAttribute('class', 'blue-car')
-document.body.appendChild($img)
+var $blueCarImage = document.createElement('img')
+$blueCarImage.setAttribute('src', 'blue-car.png')
+$blueCarImage.setAttribute('class', 'blue-car')
+document.body.appendChild($blueCarImage)
+
+var blueCar = new Car($blueCarImage, 10, 'east', [0, 0])
